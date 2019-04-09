ff5ca0bb1d0c148954235db9d2f79b568e54b46c
Andrey Karamnoff <karamnoff@gmail.com>
2019-04-09 11:06:06 +0300

    add hexlet' solution for 03-asynchronous-code

--- printer.js
+++ printer.js
@@ -1,7 +1,9 @@
 import fs from 'fs';
 
 // BEGIN (write your solution here)
-export default (filePath) => {
-  return fs.readFile(filePath, 'utf-8', (error, data) => console.log(data));
-};
+export default filepath => fs.readFile(
+  filepath,
+  'utf-8',
+  (error, data) => console.log(data),
+);
 // END
