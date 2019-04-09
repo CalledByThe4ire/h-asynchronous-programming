35a7e1e0abfdeb9549caafc4c790df840e915b73
Andrey Karamnoff <karamnoff@gmail.com>
2019-04-09 11:05:12 +0300

    add solution for 03-asynchronous-code

--- printer.js
+++ printer.js
@@ -1,5 +1,7 @@
 import fs from 'fs';
 
 // BEGIN (write your solution here)
-
+export default (filePath) => {
+  return fs.readFile(filePath, 'utf-8', (error, data) => console.log(data));
+};
 // END
