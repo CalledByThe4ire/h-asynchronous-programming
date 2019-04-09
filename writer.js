6e0e6c2ae1e9583cfbc2eb0673bbb50c0f3040c6
Andrey Karamnoff <karamnoff@gmail.com>
2019-04-09 11:07:21 +0300

    add solution for 04-asynchronous-return

--- writer.js
+++ writer.js
@@ -1,5 +1,10 @@
 import fs from 'fs';
 
 // BEGIN (write your solution here)
-
+export default (fileName, customData, cb) => {
+  fs.writeFile(fileName, customData, (_error, data) => {
+    cb(null, data);
+    console.log('success');
+  });
+};
 // END
