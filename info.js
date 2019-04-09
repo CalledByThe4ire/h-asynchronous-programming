05595d1efd6159fc98581e5c2aeaee289b59a928
Andrey Karamnoff <karamnoff@gmail.com>
2019-04-09 12:22:13 +0300

    add solution for 05-asynchronous-flow

--- info.js
+++ info.js
@@ -2,5 +2,11 @@
 import fs from 'fs';
 
 // BEGIN (write your solution here)
-
+export const compareFileSizes = (filePath1, filePath2, cb) => {
+  fs.stat(filePath1, (_error1, stat1) => {
+    fs.stat(filePath2, (_error2, stat2) => {
+      cb(null, Math.sign(stat1.size - stat2.size));
+    });
+  });
+};
 // END
