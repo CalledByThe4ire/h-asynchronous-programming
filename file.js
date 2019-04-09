72aaf465272f9693fea5f6b1e0771b143e8ee485
Andrey Karamnoff <karamnoff@gmail.com>
2019-04-09 14:05:41 +0300

    add solution for 06-error-handling

--- file.js
+++ file.js
@@ -2,5 +2,19 @@
 import fs from 'fs';
 
 // BEGIN (write your solution here)
-
+export const move = (srcPath, destPath, cb) => {
+  fs.readFile(srcPath, (_err1, data1) => {
+    if (_err1) {
+      cb(_err1);
+      return;
+    }
+    fs.writeFile(destPath, data1, (_err2) => {
+      if (_err2) {
+        cb(_err2);
+        return;
+      }
+      fs.unlink(srcPath, cb);
+    });
+  });
+};
 // END
