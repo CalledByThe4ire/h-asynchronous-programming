cc410861c2e31e4be9b586031f771ea880551c2c
Andrey Karamnoff <karamnoff@gmail.com>
2019-04-10 11:45:04 +0300

    add hexlet' solution for 08-timers

--- watcher.js
+++ watcher.js
@@ -1,5 +1,21 @@
 import fs from 'fs';
 
 // BEGIN (write your solution here)
+const check = (timerId, filepath, period, cb) => {
+  fs.stat(filepath, (err, stat) => {
+    if (err) {
+      clearInterval(timerId);
+      cb(err);
+      return;
+    }
+    if ((Date.now() - stat.mtimeMs) < period) {
+      cb(null);
+    }
+  });
+};
 
+export default (filepath, period, cb) => {
+  const timerId = setInterval(() => check(timerId, filepath, period, cb), period);
+  return timerId;
+};
 // END
