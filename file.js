89bb8cfcf7c31cd1bda939268048ecd81db97b47
Andrey Karamnoff <karamnoff@gmail.com>
2019-04-11 15:46:44 +0300

    add hexlet' solution for 09-event-loop

--- file.js
+++ file.js
@@ -3,5 +3,11 @@ import fs from 'fs';
 import { waterfall } from 'async';
 
 // BEGIN (write your solution here)
-
+export const unionFiles = (inputPath1, inputPath2, outputPath, cb) => {
+  waterfall([
+    callback => fs.readFile(inputPath1, callback),
+    (data1, callback) => fs.readFile(inputPath2, (err, data2) => callback(err, data1, data2)),
+    (data1, data2, callback) => fs.writeFile(outputPath, `${data1}${data2}`, callback),
+  ], cb);
+};
 // END
