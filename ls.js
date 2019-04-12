e4b5d589bfb31c5c672461d7adbb94c57ab6d142
Andrey Karamnoff <karamnoff@gmail.com>
2019-04-12 18:40:13 +0300

    add hexlet' solution for ls-challenge

--- ls.js
+++ ls.js
@@ -3,23 +3,16 @@ import path from 'path';
 import { promises as fs } from 'fs';
 
 // BEGIN (write your solution here)
-export default async inputPath => {
-  const inputStat = await fs.stat(inputPath);
-
-  if (!inputStat.isFile()) {
-    const fileNames = await fs.readdir(inputPath);
-    const statistics = await Promise.all(
-      fileNames.map(async fileName => {
-        const filepath = path.join(inputPath, fileName);
-        const { mode } = await fs.stat(filepath);
-        return {
-          filepath,
-          mode
-        };
-      })
-    );
-    return _.sortBy(statistics, 'filepath');
+export default async (pathForInspect) => {
+  const absolutePath = path.resolve(__dirname, pathForInspect);
+  const stat = await fs.stat(absolutePath);
+  if (stat.isFile()) { // guard expression
+    return [{ filepath: absolutePath, mode: stat.mode }];
   }
-  return [{ filepath: inputPath, mode: inputStat.mode }];
+
+  const filenames = await fs.readdir(absolutePath);
+  const filepaths = filenames.sort().map(n => path.join(absolutePath, n));
+  const stats = await Promise.all(filepaths.map(fs.stat));
+  return _.zipWith(filepaths, stats, (filepath, { mode }) => ({ filepath, mode }));
 };
 // END
