5d9ac2686f835f69e95e3dbd6e2185fcb8de4923
Andrey Karamnoff <karamnoff@gmail.com>
2019-04-12 11:35:56 +0300

    add hexlet' solution for 10-promises

--- file.js
+++ file.js
@@ -2,5 +2,6 @@
 import { promises as fs } from 'fs';
 
 // BEGIN (write your solution here)
-export const reverse = filepath => fs.readFile(filepath, 'utf-8').then(data => data.split('\n').reverse().join('\n')).then(data => fs.writeFile(filepath, data));
+export const reverse = filepath => fs.readFile(filepath, 'utf-8')
+  .then(data => fs.writeFile(filepath, data.split('\n').reverse().join('\n')));
 // END
