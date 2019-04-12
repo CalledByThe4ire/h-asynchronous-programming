c932cf2e798a4ecc97a577bf936adf63d802dafd
Andrey Karamnoff <karamnoff@gmail.com>
2019-04-12 13:33:46 +0300

    add solution for 13-promise-all

--- file.js
+++ file.js
@@ -4,5 +4,10 @@ import _ from 'lodash';
 import { promises as fs } from 'fs';
 
 // BEGIN (write your solution here)
-
+export const getDirectorySize = (dirpath) => fs.readdir(dirpath)
+  .then(filenames => {
+    const filepaths = filenames.map(filename => path.join(dirpath, filename));
+    return Promise.all(filepaths.map(filepath => fs.stat(filepath)));
+  })
+  .then(data => _.sumBy(data, 'size'));
 // END
