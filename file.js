82a009c18d1d3382bd1ade15f8528b927f5d396b
Andrey Karamnoff <karamnoff@gmail.com>
2019-04-12 15:56:24 +0300

    add solution for 15-async-await

--- file.js
+++ file.js
@@ -2,5 +2,10 @@
 import { promises as fs } from 'fs';
 
 // BEGIN (write your solution here)
-
+export const exchange = async (inputPath1, inputPath2) => {
+  const data1 = await fs.readFile(inputPath1);
+  const data2 = await fs.readFile(inputPath2);
+  await fs.writeFile(inputPath2, data1);
+  await fs.writeFile(inputPath1, data2);
+};
 // END
