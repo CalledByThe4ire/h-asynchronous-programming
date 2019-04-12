fc94e2b6a168dd2afbc75a38946ec19b2b92c57a
Andrey Karamnoff <karamnoff@gmail.com>
2019-04-12 13:34:40 +0300

    add hexlet' solution for 13-promise-all

--- file.js
+++ file.js
@@ -4,10 +4,13 @@ import _ from 'lodash';
 import { promises as fs } from 'fs';
 
 // BEGIN (write your solution here)
-export const getDirectorySize = (dirpath) => fs.readdir(dirpath)
-  .then(filenames => {
-    const filepaths = filenames.map(filename => path.join(dirpath, filename));
-    return Promise.all(filepaths.map(filepath => fs.stat(filepath)));
-  })
-  .then(data => _.sumBy(data, 'size'));
+export const getDirectorySize = (dirpath) => {
+  const promise = fs.readdir(dirpath).then((filenames) => {
+    const filepaths = filenames.map(name => path.join(dirpath, name));
+    const promises = filepaths.map(fs.stat);
+    return Promise.all(promises);
+  });
+
+  return promise.then(stats => _.sumBy(stats, 'size'));
+};
 // END
